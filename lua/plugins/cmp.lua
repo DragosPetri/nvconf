490f599323eb858e2268405b2b2f8206867b2513
Dragos Petri <dragospetri@yahoo.com>
2025-10-21 14:51:01 +0300

    update luasnip install and all deps

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -5,13 +5,10 @@ return {
     dependencies = {
       {
         "L3MON4D3/LuaSnip",
-        build = (function()
-          if vim.fn.has "win32" == 1 or vim.fn.executable "make" == 0 then
-            return
-          end
-          return "make install_jsregexp"
-        end)(),
-        dependencies = {},
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
       },
       "saadparwaiz1/cmp_luasnip",
       "hrsh7th/cmp-nvim-lsp",
